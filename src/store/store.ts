2663a946ba7b4a25c8bfef902f8773066114815c
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-01-30 14:24:32 +0545

    create custom type safe useSelector hook

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,4 +7,5 @@ export const store = configureStore({
   },
 });
 
+export type RootState = ReturnType<typeof store.getState>; // return type of function type: RootState
 export type AppDispatch = typeof store.dispatch;
