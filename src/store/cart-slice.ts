958dca433f46aea3a1681a47f25a99471bcda282
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-01-30 14:06:57 +0545

    create redux slice store

---
+++ src/store/cart-slice.ts
@@ -0,0 +1,44 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+type CardItem = {
+  id: string;
+  title: string;
+  price: number;
+  quantity: number;
+};
+type CartState = {
+  items: CardItem[];
+};
+const initialState: CartState = {
+  items: [],
+};
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(
+      state,
+      action: PayloadAction<{ id: string; title: string; price: number }>
+    ) {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (itemIndex >= 0) {
+        state.items[itemIndex].quantity++;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeFromCart(state, action: PayloadAction<string>) {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload
+      );
+      if (state.items[itemIndex].quantity === 1) {
+        state.items.splice(itemIndex, 1);
+      } else {
+        // decrese the quantity
+        state.items[itemIndex].quantity--;
+      }
+    },
+  },
+});
