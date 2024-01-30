a39f3a8d202812e33560bad7e86c2067a99bd1a1
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-01-30 11:40:29 +0545

    add basic tailwind css to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      dropShadow: {
+        text: "0 0 4px rgba(35, 34, 34, 0.4)",
+        product: "0 0 10px rgba(0, 0, 0, 0.2)",
+        cartModal: "0 0 10px rgba(0, 0, 0, 0.5)",
+      },
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        products: "repeat(auto-fit, minmax(20rem, 1fr))",
+      },
+      animation: {
+        cartmodal: "fade-slide-in-from-top 0.3s ease-in-out",
+        checkout: "fade-slide-in-from-right 0.3s ease-in-out",
+      },
+      keyframes: {
+        cartmodal: {
+          "0%": { opacity: 0, transform: "translateY(-50px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+        checkout: {
+          "0%": { opacity: 0, transform: "translateX(50px)" },
+          "100%": { opacity: 1, transform: "translateX(0)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
